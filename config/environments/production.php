f078f6dbdf1a5797761e58ee9c7b808d7273a464
Mohsen Derogar <78999228+mohsendro@users.noreply.github.com>
2026-09-15 13:03:58 +0330

    chore: add starter file config/environments/production.php

---
+++ config/environments/production.php
@@ -0,0 +1,7 @@
+<?php
+
+Config::define('WP_DEBUG', false);
+Config::define('WP_DEBUG_LOG', false);
+Config::define('WP_DEBUG_DISPLAY', false);
+Config::define('DISALLOW_FILE_EDIT', true);
+Config::define('DISALLOW_FILE_MODS', false);
